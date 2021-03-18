041dd33f8a9d3c0d1dc9ba21a174bddebb76bdd6
regularavali <80402237+regularavali@users.noreply.github.com>
2021-03-18 18:01:09 +0530

    Add files via upload

---
+++ new.php
@@ -0,0 +1,14 @@
+<?php
+$firstname=$_POST["firstname"];
+$surname=$_POST["surname"];
+$mobilenumber=$_POST["mobilenumber"];
+$emailid=$_POST["emailid"];
+$newpassword=$_POST["newpassword"];
+echo $firstname;
+echo "</br>";
+echo $surname;
+echo "</br>";
+echo $mobilenumber;
+echo "</br>";
+echo $emailid;
+?>
